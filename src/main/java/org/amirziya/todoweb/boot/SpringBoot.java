98529b03850c38b91b10b9ca7c01c1c7172c7279
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2024-10-18 21:06:17 +0330

    change-todo-model

--- src/main/java/org/amirziya/todoweb/boot/SpringBoot.java
+++ src/main/java/org/amirziya/todoweb/boot/SpringBoot.java
@@ -2,7 +2,6 @@ package org.amirziya.todoweb.boot;
 
 import lombok.RequiredArgsConstructor;
 import org.amirziya.todoweb.model.Todo;
-import org.amirziya.todoweb.repository.TodoRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,20 +9,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @RequiredArgsConstructor
 public class SpringBoot implements CommandLineRunner {
-    private final TodoRepository todoRepository;
-
     @Transactional
     @Override
     public void run(String... args) throws Exception {
-//        loadTodos();
+        loadTodos();
     }
-
     public void loadTodos() {
         Todo todo = Todo.builder()
                 .description("This is a todo")
                 .title("Personal")
                 .build();
-        todoRepository.save(todo);
     }
 
 
--- src/main/java/org/amirziya/todoweb/model/Todo.java
+++ src/main/java/org/amirziya/todoweb/model/Todo.java
@@ -4,11 +4,13 @@ package org.amirziya.todoweb.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @Builder
 public class Todo {
 
-    private  Long id;
+    private UUID id;
     private String title;
     private String description;
     private boolean isDO;
