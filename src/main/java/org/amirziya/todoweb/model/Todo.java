5cbe78203dfc99b1766bddf0f671df74ae33aae0
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2024-10-18 13:34:20 +0330

    add-todo-model

---
+++ src/main/java/org/amirziya/todoweb/model/Todo.java
@@ -0,0 +1,18 @@
+package org.amirziya.todoweb.model;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Todo {
+
+    private  Long id;
+
+    private String title;
+
+    private String Description;
+
+    private boolean isDO;
+}
