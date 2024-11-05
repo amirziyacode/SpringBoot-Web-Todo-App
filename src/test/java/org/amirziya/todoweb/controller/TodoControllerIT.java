07da698ef70548c2bd621155406b1beeab8e1764
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2024-11-05 19:42:55 +0330

    add-test-for-get-and-save-todos

--- src/test/java/org/amirziya/todoweb/controller/TodoControllerIT.java
+++ src/test/java/org/amirziya/todoweb/controller/TodoControllerIT.java
@@ -3,11 +3,11 @@ package org.amirziya.todoweb.controller;
 import jakarta.transaction.Transactional;
 import org.amirziya.todoweb.model.Todo;
 import org.amirziya.todoweb.repo.TodoRepo;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,6 +15,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class TodoControllerIT {
@@ -34,11 +36,34 @@ class TodoControllerIT {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
-
     @Test
     void get_all_todos(){
         List<Todo> all = todoRepo.findAll();
-        Assertions.assertThat(all.size()).isEqualTo(3);
+        assertThat(all.size()).isEqualTo(3);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void save_Todo_in_db(){
+        Todo newTodo = Todo.builder()
+                .title("Test")
+                .description("This is Todo !!")
+                .isDO(false)
+                .build();
+
+        ResponseEntity<Todo> todo = todoController.createTodo(newTodo);
+
+        assertThat(todo.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(201));
+        assertThat(todo.getHeaders().getLocation()).isNotNull();
+    }
+
+    @Test
+    void get_Todo_Id(){
+        Todo first = todoRepo.findAll().get(0);
+        ResponseEntity<Optional<Todo>> todoById = todoController.getTodoById(first.getId());
+
+        assertThat(todoById).isNotNull();
     }
 
     @Rollback
@@ -47,7 +72,7 @@ class TodoControllerIT {
     void empty_Data(){
         todoRepo.deleteAll();
         ResponseEntity<List<Todo>> listResponseEntity = todoController.listTodo();
-        Assertions.assertThat(Objects.requireNonNull(listResponseEntity.getBody()).size()).isEqualTo(0);
+        assertThat(Objects.requireNonNull(listResponseEntity.getBody()).size()).isEqualTo(0);
     }
 
 
